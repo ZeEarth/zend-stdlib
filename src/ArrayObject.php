72671a6042265b26860fc328ced0a853579496ab
ZeEarth <zeearth@gmail.com>
2019-03-09 17:01:12 +0100

    Update ArrayObject.php
    Correct break for continue to php 7.3 compatibility

--- src/ArrayObject.php
+++ src/ArrayObject.php
@@ -417,15 +417,15 @@ class ArrayObject implements IteratorAggregate, ArrayAccess, Serializable, Count
             switch ($k) {
                 case 'flag':
                     $this->setFlags($v);
-                    break;
+                    continue;
                 case 'storage':
                     $this->exchangeArray($v);
-                    break;
+                    continue;
                 case 'iteratorClass':
                     $this->setIteratorClass($v);
-                    break;
+                    continue;
                 case 'protectedProperties':
-                    break;
+                    continue;
                 default:
                     $this->__set($k, $v);
             }
